6528ddd16eb10e7f9a6fecdd8ede1d833027b049
Ziyad <ziyad.ibrahim.112358@gmail.com>
2023-12-27 05:53:55 -0800

    Simple shell 0.1

--- simple_shell.c
+++ simple_shell.c
@@ -5,11 +5,8 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-extern char **environ;
-
-
 void
-run_non_interactive()
+run_non_interactive(char **env)
 {
 	char **cpargv;
 	ssize_t nread;
@@ -27,9 +24,9 @@ run_non_interactive()
 			wait(&status);
 		else
 		{
-			execve(strtok(cpargv[0], " "), cpargv, environ);
-			dprintf(STDERR_FILENO ,
-				"%s: No such file or directory\n", cpargv[0]);
+			execve(strtok(cpargv[0], " "), cpargv, env);
+			dprintf(STDERR_FILENO
+				, "%s: No such file or directory\n", cpargv[0]);
 			free(cpargv[0]);
 			free(cpargv);
 			exit(EXIT_FAILURE);
@@ -44,21 +41,23 @@ run_non_interactive()
 int
 main(__attribute__((unused)) int ac, char **argv, char **env)
 {
-	char **param = malloc(sizeof(char *) * 2);
+	char **param;
 	ssize_t nread;
 	size_t len;
 	int status;
 
 	if (!isatty(STDIN_FILENO))
-		run_non_interactive();
+		run_non_interactive(env);
 
+	param = malloc(sizeof(char *) * 2);
 	param[1] = NULL;
 	while (1)
 	{
 		printf("#cisfun$ ");
 		fflush(stdout);
 
-		if((nread = getline(&param[0], &len, stdin)) == -1)
+		nread = getline(&param[0], &len, stdin);
+		if (nread == -1)
 			break;
 
 		(param[0])[nread - 1] = '\0';
@@ -68,8 +67,8 @@ main(__attribute__((unused)) int ac, char **argv, char **env)
 		else
 		{
 			execve(strtok(param[0], " "), param, env);
-			dprintf(STDERR_FILENO ,
-				"%s: No such file or directory\n", argv[0]);
+			dprintf(STDERR_FILENO
+				, "%s: No such file or directory\n", argv[0]);
 			exit(EXIT_FAILURE);
 		}
 	}
